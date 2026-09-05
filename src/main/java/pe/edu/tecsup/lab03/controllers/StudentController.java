b84a6fe48f196e7e607a1f243de8e3ac7e062975
Jhordan 2 <jhordan.sullca@tecsup.edu.pe>
2026-09-05 18:24:02 -0500

    second commit

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -2,6 +2,8 @@ package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
     public void getAllStudents() {
+
         System.out.println("Listando estudiantes desde el controlador...");
+        //Modificado en Sprint2 por colaborador p
     }
 }
--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -3,6 +3,9 @@ package pe.edu.tecsup.lab03.entities;
 public class StudentEntity {
     private Long id;
     private String name;
+    private  String email;
+    //Modificado en Sprint2 por colaborador p
+
 
     public StudentEntity() {}
 }
