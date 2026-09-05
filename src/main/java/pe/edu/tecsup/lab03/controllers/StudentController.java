c1c421a5594529e455d7a112201c624d65230848
Jhordan <mariusjhordan@gmail.com>
2026-09-05 18:27:21 -0500

    third commit

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -3,5 +3,6 @@ package pe.edu.tecsup.lab03.controllers;
 public class StudentController {
     public void getAllStudents() {
         System.out.println("Listando estudiantes desde el controlador...");
+        System.out.println("Cambio Sprint 2 - Responsable");
     }
 }
